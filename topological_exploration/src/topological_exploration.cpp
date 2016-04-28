25e03964d7cc172f745aecd9947ed13ce074fe2c
Joao Santos <jsantos@lincoln.ac.uk>
2016-04-28 15:08:19 +0100

    dubuggin

--- topological_exploration/src/topological_exploration.cpp
+++ topological_exploration/src/topological_exploration.cpp
@@ -331,7 +331,7 @@ int generateNewSchedule(uint32_t givenTime)//TODO -> save schedule in MongoDB
     int numSlots = 24*3600/windowDuration;
     uint32_t timeSlots[numSlots];
     uint32_t midnight = getMidnightTime(givenTime);
-    //retrieveGrids(midnight);
+    retrieveGrids(midnight);
 
     /*create timeslots*/
     for (int i = 0;i<numSlots;i++) timeSlots[i] = midnight+3600*24/numSlots*i;
@@ -355,10 +355,7 @@ int generateNewSchedule(uint32_t givenTime)//TODO -> save schedule in MongoDB
             times[0] = timeSlots[s];
             coordinateSearch(fremengridSet.fremengrid[i]->id, &observationPoint);
             entropy[0] = fremengridSet.estimateEntropy(fremengridSet.fremengrid[i]->id, observationPoint.x, observationPoint.y, 1.6, camera_range, times[0]);
-
-            if(entropy[0]  = 0.0)
-                ROS_INFO("ID: %s\t Point: %f %f %f\t Entropy: %f", fremengridSet.fremengrid[i]->id,observationPoint.x, observationPoint.y,observationPoint.z,entropy[0]);
-
+            ROS_INFO("ID: %s\t Point: %f %f %f\t Entropy: %f", fremengridSet.fremengrid[i]->id,observationPoint.x, observationPoint.y,observationPointentropy[0]);
             lastWheel += explorationRatio*entropy[0];
             wheel[i] = lastWheel;
         }
