3f48a61dd0b0020118d5324413f96e0520fd750c
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2016-05-03 14:08:01 +0100

    small edits

--- exploration_tasks_generator/scripts/fake_edge_exploration.py
+++ exploration_tasks_generator/scripts/fake_edge_exploration.py
@@ -1,18 +1,29 @@
 #! /usr/bin/env python
 
+import random
 import rospy
 
 from strands_exploration_msgs.srv import GetExplorationTasks, GetExplorationTasksResponse
+from strands_navigation_msgs.srv import GetTopologicalMap
 
 
 class FakeEdgeExploration(object):
 
     def __init__(self):
-        rospy.Service("edge_exp_srv", GetExplorationTasks, self.service_cb)
+        self.get_top_map_srv=rospy.ServiceProxy("/topological_map_publisher/get_topological_map", GetTopologicalMap)
+        rospy.Service("/exploration_services/edge_exp_srv", GetExplorationTasks, self.service_cb)
         
     def service_cb(self, request):
-        return GetExplorationTasksResponse(task_definition=['WayPoint19_WayPoint20', 'WayPoint21_WayPoint22'],
-                                           task_score=[0.8,0.3])
+        res=GetExplorationTasksResponse(task_definition=[],
+                                           task_score=[])
+        topo_map=self.get_top_map_srv(rospy.get_param("/topological_map_name")).map
+        for i in range(0,8):
+            node=random.choice(topo_map.nodes)
+            if node.name != "ChargingPoint":
+                edge=random.choice(node.edges).edge_id
+                res.task_definition.append(edge)
+                res.task_score.append(random.random())
+        return res
                     
 
     def main(self):
