0de3a462697dbe48bf6b1d02b4d9c7645f9a6715
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2016-05-03 14:07:53 +0100

    stop using rosservice.rosservice_find because its not robust to hanging nodes; use do_sweep action

--- exploration_tasks_generator/src/exploration_tasks_generator/exploration_services_manager.py
+++ exploration_tasks_generator/src/exploration_tasks_generator/exploration_services_manager.py
@@ -19,12 +19,14 @@ class ExplorationServicesManager(object):
         
     def generate_tasks(self, start_time, end_time):
         task_list=[]
-        exploration_service_names=rosservice.rosservice_find("strands_exploration_msgs/GetExplorationTasks")
+        exploration_service_names=rosservice.get_service_list(namespace="/exploration_services")
+        print exploration_service_names
         exploration_service_list=[rospy.ServiceProxy(name, GetExplorationTasks) for name in exploration_service_names]
         for (name, service) in zip(exploration_service_names, exploration_service_list):
             response=service(start_time, end_time)
-            no_slash_name=name.replace('/', '_')
-            method_name="create_tasks_for" + no_slash_name
+            service_name=name.split('/')[-1]
+            print service_name
+            method_name="create_tasks_for_" + service_name
             if hasattr(self, method_name) and callable(getattr(self, method_name)):
                 method=getattr(self, method_name)
                 task_list+=method(response.task_definition, response.task_score, start_time, end_time)
@@ -49,12 +51,13 @@ class ExplorationServicesManager(object):
     def create_tasks_for_fremen_grid_exp_srv(self, definitions, scores, start_time, end_time):
         res=[]
         for (wp, score) in zip(definitions, scores):
-            task=Task(action= 'ptu_pan_tilt_metric_map',
+            task=Task(action= 'do_sweep',
                       start_node_id=wp,
                       end_node_id=wp,
                       start_after=start_time,
                       end_before=end_time,
-                      max_duration=rospy.Duration(4*60))
+                      max_duration=rospy.Duration(1*60))
+            tu.add_string_argument(task, 'medium')
             res.append(ExplorationTask(task_type="fremen_grid_exploration",
                                        score=score,
                                        task_def=task))
