94d78c10d7f799f5c9d027e689d50c2a85a8a8c8
Rares <raambrus@kth.se>
2016-05-04 21:21:05 +0100

    fixing problem with region-wp correspondence

--- activity_exploration/scripts/counting_manager.py
+++ activity_exploration/scripts/counting_manager.py
@@ -4,12 +4,13 @@
 import rospy
 import random
 from std_msgs.msg import String
+from region_observation.util import is_intersected
 from strands_navigation_msgs.msg import TopologicalMap
+from strands_exploration_msgs.srv import GetExplorationTasks
 from region_observation.util import robot_view_cone, get_soma_info
+from strands_exploration_msgs.srv import GetExplorationTasksResponse
 from periodic_poisson_processes.poisson_wrapper import PoissonWrapper
 from periodic_poisson_processes.people_poisson import PoissonProcessesPeople
-from region_observation.util import is_intersected, get_largest_intersected_regions
-from strands_exploration_msgs.srv import GetExplorationTasks, GetExplorationTasksResponse
 
 
 class PeopleCountingManager(object):
@@ -24,17 +25,23 @@ class PeopleCountingManager(object):
         )
         self.poisson_proc.load_from_db()
         self.poisson_consent = PoissonWrapper(
-            rospy.get_param("~consent_topic", "/skeleton_data/consent_ret"), String,
-            "data", "nothing", window=time_window*3, increment=time_increment,
-            periodic_cycle=periodic_cycle/7
+            rospy.get_param("~consent_topic", "/skeleton_data/consent_ret"),
+            # "data", "nothing", window=time_window*3, increment=time_increment,
+            # periodic_cycle=periodic_cycle/7
+            String, "data", "nothing", 2, 1, 3
         )
         rospy.sleep(0.1)
         self.topo_map = None
         self.region_wps = self._get_waypoints(soma_config)
-        rospy.loginfo("Region ids and their nearest waypoints: %s" % str(self.region_wps))
+        rospy.loginfo(
+            "Region ids and their nearest waypoints: %s" % str(self.region_wps)
+        )
         rospy.sleep(0.1)
         rospy.loginfo("Create a service %s/get_waypoints..." % name)
-        self.service = rospy.Service('/exploration_services/activity_exp_srv', GetExplorationTasks, self._srv_cb)
+        self.service = rospy.Service(
+            '/exploration_services/activity_exp_srv',
+            GetExplorationTasks, self._srv_cb
+        )
         rospy.sleep(0.1)
 
     def spin(self):
@@ -49,7 +56,7 @@ class PeopleCountingManager(object):
         time_window = rospy.get_param("~time_window", 10)
         if (msg.end_time - msg.start_time).secs < time_window*60:
             rospy.logwarn(
-                "Time window between start and end time is too small, widening the time window..."
+                "Delta start and end time is small, widening the time window..."
             )
             msg.end_time = msg.start_time + rospy.Duration(time_window*60)
         rates = self.poisson_proc.retrieve_from_to(
@@ -60,18 +67,29 @@ class PeopleCountingManager(object):
             total_rate = sum(poisson.values())
             result.append((total_rate, roi))
         result = sorted(result, key=lambda i: i[0], reverse=True)
-        total = float(sum([i[0] for i in result]))
+        suggested_wps = list()
+        suggested_score = list()
+        for i in result:
+            if i[1] in self.region_wps:
+                suggested_wps.append(self.region_wps[i[1]])
+                suggested_score.append(i[0])
+            else:
+                rospy.loginfo(
+                    "No waypoint covers region %s, removing region..." % i[1]
+                )
+        total = float(sum(suggested_score))
         try:
             task = GetExplorationTasksResponse(
-                map(lambda i: self.region_wps[i], [i[1] for i in result])[:5],
-                map(lambda i: i/total, [i[0] for i in result])[:5]
+                suggested_wps,
+                map(lambda i: i/total, suggested_score)[:5]
             )
         except:
-            temp = map(lambda i: self.region_wps[i], [i[1] for i in result])[:5]
-            rospy.logwarn("Ups...no suggestion for waypoints is found at the moment")
+            rospy.logwarn(
+                "Ups...no suggestion for waypoints is found at the moment"
+            )
             task = GetExplorationTasksResponse(
-                temp,
-                [1/float(len(temp)) for i in range(len(temp))]
+                suggested_wps,
+                [1/float(len(suggested_wps)) for i in range(len(suggested_wps))]
             )
         task = self._check_consent(msg, task)
         print task
@@ -83,10 +101,11 @@ class PeopleCountingManager(object):
         )
         if sum(rates_consent.values()) > rospy.get_param("~consent_rate", 1.5):
             rospy.loginfo("Waypoint's order: %s" % str(task.task_definition))
-            rospy.logwarn("Consent rate shows too many rejections with this configuration...")
             rospy.logwarn("Shuffling suggested waypoints...")
             random.shuffle(task.task_definition)
-            rospy.loginfo("New waypoint's order: %s" % str(task.task_definition))
+            rospy.loginfo(
+                "New waypoint's order: %s" % str(task.task_definition)
+            )
         return task
 
     def _topo_map_cb(self, topo_map):
@@ -114,26 +133,22 @@ class PeopleCountingManager(object):
                     intersected_regions.append(region)
                     intersected_rois.append(roi)
             if len(intersected_regions) > 1:
-                rospy.logwarn(
-                    "There are two or more regions covered by the sight of the robot in %s" % wp.name
-                )
-                rospy.loginfo("Trying to get the largest intersected area between robot's sight and regions")
-                region = get_largest_intersected_regions(wp_sight, intersected_regions)
-                roi = intersected_rois[intersected_regions.index(region)]
-            elif len(intersected_regions) == 1:
-                region = intersected_regions[0]
-                roi = intersected_rois[0]
-            else:
-                rospy.logwarn("No region is covered by %s!" % wp.name)
+                rospy.loginfo("%s covers more than one region..." % wp.name)
+            elif len(intersected_regions) == 0:
+                rospy.logwarn("%s does not cover any region!" % wp.name)
                 continue
-            area = wp_sight.intersection(region).area
-            if roi in region_wps:
-                _, area1 = region_wps[roi]
-                if area > area1:
+            for ind, region in enumerate(intersected_regions):
+                area = wp_sight.intersection(region).area
+                roi = intersected_rois[ind]
+                if roi in region_wps:
+                    _, area1 = region_wps[roi]
+                    if area > area1:
+                        region_wps.update({roi: (wp.name, area)})
+                else:
                     region_wps.update({roi: (wp.name, area)})
-            else:
-                region_wps.update({roi: (wp.name, area)})
-        return {roi: tupleoftwo[0] for roi, tupleoftwo in region_wps.iteritems()}
+        return {
+            roi: tupleoftwo[0] for roi, tupleoftwo in region_wps.iteritems()
+        }
 
 
 if __name__ == '__main__':
