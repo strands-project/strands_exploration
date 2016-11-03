45d44ddb5ea2ad170a778c9b9db6d13ceab9004d
Joao Santos <jsantos@lincoln.ac.uk>
2016-11-03 09:08:02 +0000

    bids every two hours on the most entropic tgimeslot

--- spatiotemporal_exploration/src/spatiotemporal_bidder.py
+++ spatiotemporal_exploration/src/spatiotemporal_bidder.py
@@ -16,82 +16,71 @@ class SpatioTemporalBidder(object):
         self.slot_durantion = 0;
         self.num_slots = 0;
         
-        rospy.Timer(rospy.Duration(60), self.add_task)
+        #every 2 hours checks bucget and bids
+        rospy.Timer(rospy.Duration(7200), self.add_task)
         rospy.Subscriber("/spatiotemporal_exploration/exploration_schedule", ExplorationSchedule, self.schedule_listener)
     
-        ##DEBUGGING SERVICES
-        #rospy.Service("add_budget", Empty, self.add_budget)
-        #rospy.Service("bid", Empty, self.bid)
-        #rospy.Service("get_info", Empty, self.get_info)
-        
+
     def schedule_listener(self, data):
-        self.exploration_schedule = data 
-        
-        self.slot_duration = self.exploration_schedule.timeInfo[2] - self.exploration_schedule.timeInfo[1]
+        self.exploration_schedule = data
         self.num_slots  = len(data.timeInfo)
         #some debug messages:        
-        print "Nr slots: ", self.num_slots 
-        print "Time slot duration: ", self.slot_durantion
+        print "Received schedule -- nr of time slots: ", self.num_slots 
+        
            
-#        self.schedule_sorted = []
-#        for i in range(len(data.timeInfo)):
-#            e={}
-#            self.e['timeInfo']=data.timeInfo[i]
-#            self.e['nodeID']=data.nodeID[i]
-#            self.e['entropy']=data.entropy[i]
-#            self.schedule_sorted.append(e)
-
+    def getCurrentTimeSlot(self, slot_duration):
+        
+        numSlots = 24*3600/slot_duration;
+        now = rospy.get_rostime().secs
+        
+        currentSlot = (now-self.exploration_schedule.timeInfo[0])/slot_duration;
+        
+        if currentSlot > numSlots:
+            currentSlot = numSlots
+        
+        return currentSlot
+        
     def add_task(self, timer_event):
         print self.bidder.available_tokens
         print self.bidder.currently_bid_tokens
-        task=Task(action= 'do_sweep',
-                        start_node_id='CorpLocker6',
-                        end_node_id='CorpLocker6',
-                        start_after=rospy.get_rostime(),
-                        end_before=rospy.get_rostime() + rospy.Duration(60*1),
-                        max_duration=rospy.Duration(1*60))
-        bid = int(ceil(0.1*(self.bidder.available_tokens- self.bidder.currently_bid_tokens)))
-        self.bidder.add_task_bid(task, bid)
+                
+                
+        if len(self.exploration_schedule.timeInfo) == 0:
+            print " exploration schedule no yet received"
+        else:
+            self.slot_duration = self.exploration_schedule.timeInfo[2] - self.exploration_schedule.timeInfo[1]
+            print "Time slot duration: ", self.slot_duration
+
+            #get current time slot:            
+            nextSlot = self.getCurrentTimeSlot(self.slot_duration) + 1
+            lookAhead = 7200/self.slot_duration
+            
+            maxSlot = nextSlot + lookAhead
+            if maxSlot - nextSlot > 1:             
+                #reorder slots based on the entropy for each 2h interval
+                for i in range(nextSlot, maxSlot):
+                    self.schedule_sorted = []
+                    for i in range():
+                        e={}
+                        self.e['timeInfo']=self.exploration_schedule.timeInfo[i]
+                        self.e['nodeID']=self.exploration_schedule.nodeID[i]
+                        self.e['entropy']=self.exploration_schedule.entropy[i]
+                self.schedule_sorted.append(e)
+                                
+                task=Task(action= 'do_sweep',
+                        start_node_id=self.schedule_sorted.nodeID[0],
+                        end_node_id=self.schedule_sorted.nodeID[0],
+                        start_after=self.schedule_sorted.timeInfo[0],
+                        end_before=self.schedule_sorted.timeInfo[0] + self.slot_duration,
+                        max_duration=self.slot_duration)
+                        
+                bidRatio = (self.num_slots - nextSlot)/self.slot_duration        
+                bid = int(ceil((self.bidder.available_tokens- self.bidder.currently_bid_tokens)/bidRatio))
+                self.bidder.add_task_bid(task, bid)
         
-    #DEBUGGING SERVICES    
-    #def add_budget(self, req):
-        #self.bidder.available_tokens+=20
-        #self.bidder.process_task_queue()
-        #return []
-    
-    #def bid(self, req):
-        #task=Task(action= 'do_sweep',
-                        #start_node_id='CorpLocker6',
-                        #end_node_id='CorpLocker6',
-                        #start_after=rospy.get_rostime(),
-                        #end_before=rospy.get_rostime() + rospy.Duration(60*1),
-                        #max_duration=rospy.Duration(2*60))
-        #bid = 10
-        #self.bidder.add_task_bid(task, bid)
-        #return []
-        
-        
-    #def get_info(self, req):
-        #print "AVAILABLE TOKENS: ", self.bidder.available_tokens
-        #print "BID TOKENS: ", self.bidder.currently_bid_tokens
-        #print "ADDED TOKENS: ", self.bidder.currently_added_tokens
-        #print "ADDED TASKS: ", self.bidder.added_tasks
-        #print "--------------------------------------------------------------------------------------------------------------------"
-        #print "QUEUED TASJS: ", self.bidder.queued_tasks
-        #print "\n\n\n\n\n\n\n"
-        #return []
      
     def main(self):
         
-        #split the day in 4 tranches
-        # for each tranche select 
-        slots_tranches = self.num_slots/4 
-        print "nr slots per tranch: ", slots_tranches
-
-        for i in xrange(4):
-            print "tranche: ", i
-            for j in range(i*slots_tranches, i*slots_tranches+slots_tranches,1):
-                tranch_budget = self.bidder.available_tokens
        # Wait for control-c
         rospy.spin()       
 
