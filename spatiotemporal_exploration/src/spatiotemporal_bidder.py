759f242953453a2a9ee3bf4ec1ede8c90fd697e9
Joao Santos <jsantos@lincoln.ac.uk>
2016-11-10 17:29:48 +0000

    fixing timestamps when adding a new task

--- spatiotemporal_exploration/src/spatiotemporal_bidder.py
+++ spatiotemporal_exploration/src/spatiotemporal_bidder.py
@@ -6,7 +6,7 @@ from math import ceil
 from strands_executive_msgs.msg import Task
 from exploration_bid_manager.exploration_bidder import ExplorationBidder
 from strands_exploration_msgs.msg import ExplorationSchedule
-from mongodb_store.message_store import  StringPair
+from strands_executive_msgs import task_utils
 
 class SpatioTemporalBidder(object):
 
@@ -28,6 +28,7 @@ class SpatioTemporalBidder(object):
         self.num_slots  = len(data.timeInfo)
         #some debug messages:        
         print "Received schedule -- nr of time slots: ", self.num_slots 
+        self.add_task('foo', test='bar')
         
            
     def getCurrentTimeSlot(self, slot_duration):
@@ -42,11 +43,11 @@ class SpatioTemporalBidder(object):
         
         return currentSlot
         
-    def add_task(self, timer_event):
-        print self.bidder.available_tokens
-        print self.bidder.currently_bid_tokens
+    def add_task(self, *args, **kwargs):
+#        print "Availabe tokens: ", self.bidder.available_tokens
+#        print "Currently bid tokens: ", self.bidder.currently_bid_tokens
                 
-                
+        
         if len(self.exploration_schedule.timeInfo) == 0:
             print " exploration schedule no yet received"
         else:
@@ -55,7 +56,9 @@ class SpatioTemporalBidder(object):
 
             #get current time slot:            
             nextSlot = self.getCurrentTimeSlot(self.slot_duration) + 1
+            print "nextSlot: ", nextSlot
             lookAhead = self.bidder_timer/self.slot_duration
+            print "lookAhead: ", lookAhead
             
             maxSlot = nextSlot + lookAhead
             if maxSlot - nextSlot > 1:        
@@ -70,21 +73,26 @@ class SpatioTemporalBidder(object):
                     self.schedule_sorted.append(e)
                     
                 self.schedule_sorted = sorted(self.schedule_sorted, key=lambda k:k['entropy'])
-
-                taskArg = StringPair()
-                taskArg.second = "complete"
                 
+                start_time = rospy.Time(secs = self.schedule_sorted[-1]['timeInfo'], nsecs = 0)
+                end_time = rospy.Time(secs = self.schedule_sorted[-1]['timeInfo'] + self.slot_duration, nsecs = 0)
+                task_duration = rospy.Duration(secs = self.bidder_timer)                
                 task=Task(action= 'do_sweep',
-                        start_node_id=self.schedule_sorted[-1].nodeID,
-                        end_node_id=self.schedule_sorted[-1].nodeID,
-                        start_after=self.schedule_sorted[-1].timeInfo,
-                        end_before=self.schedule_sorted[-1].timeInfo + self.slot_duration,
-                        max_duration=self.slot_duration,
-                        arguments = taskArg)
+                        start_node_id=self.schedule_sorted[-1]['nodeID'],
+                        end_node_id=self.schedule_sorted[-1]['nodeID'],
+                        start_after=start_time,
+                        end_before=end_time,
+                        max_duration=task_duration,
+                        )
+                        
+                task_utils.add_string_argument(task, 'complete')
                                         
-                bidRatio = (self.num_slots - nextSlot)/self.bidder_timer        
-                bid = int(ceil((self.bidder.available_tokens - self.bidder.currently_bid_tokens)*bidRatio))
-                self.bidder.add_task_bid(task, bid)
+                bidRatio = (self.num_slots - nextSlot)/(self.bidder_timer/self.slot_duration)
+                bid = int(ceil((self.bidder.available_tokens - self.bidder.currently_bid_tokens)/bidRatio))
+                
+                if bid > 0:
+                    print "Bid Amount: ", bid, "Node: ", self.schedule_sorted[-1]['nodeID'], "Time: ", self.schedule_sorted[-1]['timeInfo'] 
+                    self.bidder.add_task_bid(task, bid)
         
      
     def main(self):
